a0371199dfbef8168f365adf3662deb98e2faa80
casperhart <casperhart93@gmail.com>
2021-07-04 11:17:00 +1200

    display_scatter make points circular and fix size issue

--- srcts/scatter_widget/shaders.ts
+++ srcts/scatter_widget/shaders.ts
@@ -1,6 +1,14 @@
 export const FRAGMENT_SHADER = `
+varying float vSize;
+
 void main(){
     gl_FragColor = vec4(1.);
+
+    // make points circular
+    float distance = length(2.0 * gl_PointCoord - 1.0);
+    if (distance > 1.0) {
+        discard;
+    }
 }
 `
 
@@ -8,7 +16,8 @@ export const VERTEX_SHADER = `
 uniform float size;
 
 void main(){
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0);
-    gl_PointSize=50.0*size;
+    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0);
+    gl_Position = projectionMatrix * mvPosition;
+    gl_PointSize = 200.0 * size / -mvPosition.z;
 }
 `
