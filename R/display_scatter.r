7f5674ac9bf21e7d143c0219c013597c784d605d
casperhart <casperhart93@gmail.com>
2021-07-10 14:14:33 +1200

    documentation improvements

--- R/display_scatter.r
+++ R/display_scatter.r
@@ -1,3 +1,13 @@
+
+#' Animate a tour path.
+#'
+#' Display method for a high performance 3D scatterplot.
+#' Performance is achieved through the use of Three.js / WebGL.
+#' @param center If TRUE, center the projected data to (0, 0, 0).
+#' @param cache_frames if TRUE, cache projected data at each frame of the animation. Reduces CPU usage if the tour is cycling, but can increase memory usage considerably.
+#' @export
+#' @examples
+#' animate_tour(tourr::flea, -species, tourr::grand_tour(3), display_scatter())
 display_scatter <- function(center = TRUE, cache_frames = TRUE) {
     init <- function(data) {
         list(
