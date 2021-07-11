2e802679040b40b4186aa68586a63c873dbdd687
casperhart <casperhart93@gmail.com>
2021-07-11 12:30:38 +1200

    display_scatter use more efficient centering implementation
    Reduced scripting time of visual by approx. 25%

--- srcts/scatter_widget/utils.ts
+++ srcts/scatter_widget/utils.ts
@@ -22,18 +22,17 @@ export function multiply(a: Matrix, b: ProjectionMatrix): Matrix {
 }
 
 // TODO: add tests
-function getColMeans(X: Matrix): number[] {
+export function getColMeans(X: Matrix): Matrix {
     let n = X.length
     let colSums = X.reduce((a, b) => {
         return a.map((val, ind) => val + b[ind])
     })
-    return colSums.map(val => val / n)
+    return [colSums.map(val => val / n)]
 }
 
-export function centerColumns(X: Matrix): Matrix {
-    let colMeans = getColMeans(X)
+export function centerColumns(X: Matrix, colMeans: Matrix): Matrix {
     let centered = X.map(function (row: number[]) {
-        return row.map((element, index) => element - colMeans[index])
+        return row.map((element, index) => element - colMeans[0][index])
     });
     return centered
 }
