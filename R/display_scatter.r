fb1720c0615a92e42068c8f094de96cdc4f309f3
casperhart <casperhart93@gmail.com>
2021-09-05 10:46:44 +1200

    display_scatter remove cache_frames option

--- R/display_scatter.r
+++ R/display_scatter.r
@@ -5,11 +5,10 @@
 #' Performance is achieved through the use of Three.js / WebGL.
 #' @param mapping mapping created via `tour_aes()`. Currently only supports colour mapping.
 #' @param center If TRUE, center the projected data to (0, 0, 0).
-#' @param cache_frames if TRUE, cache projected data at each frame of the animation. Reduces CPU usage if the tour is cycling, but can increase memory usage considerably.
 #' @export
 #' @examples
 #' animate_tour(tourr::flea, -species, tourr::grand_tour(3), display_scatter())
-display_scatter <- function(mapping = NULL, center = TRUE, cache_frames = TRUE) {
+display_scatter <- function(mapping = NULL, center = TRUE) {
     init <- function(data) {
         default_mapping <- list(colour = character(0))
         mapping <- purrr::map(mapping, get_mapping_cols, data)
@@ -22,7 +21,7 @@ display_scatter <- function(mapping = NULL, center = TRUE, cache_frames = TRUE) 
 
         list(
             "mapping" = mapping,
-            "plot" = list("center" = center, "cacheFrames" = cache_frames),
+            "plot" = list("center" = center),
             "widget" = "display_scatter"
         )
     }
