67e638d8f8875528f30f6d5894beef6b3bf6f358
casperhart <casperhart93@gmail.com>
2022-01-23 17:09:29 +1300

    📝 vignette on how to add a new widget

---
+++ R/reexports-tourr.R
@@ -0,0 +1,38 @@
+# tour paths ------------------------------------------------------------------
+
+#' @importFrom tourr dependence_tour
+#' @export
+tourr::dependence_tour
+
+#' @importFrom tourr frozen_tour
+#' @export
+tourr::frozen_tour
+
+#' @importFrom tourr frozen_guided_tour
+#' @export
+tourr::frozen_guided_tour
+
+#' @importFrom tourr grand_tour
+#' @export
+tourr::grand_tour
+
+#' @importFrom tourr guided_section_tour
+#' @export
+tourr::guided_section_tour
+
+#' @importFrom tourr guided_tour
+#' @export
+tourr::guided_tour
+
+#' @importFrom tourr little_tour
+#' @export
+tourr::little_tour
+
+#' @importFrom tourr local_tour
+#' @export
+tourr::local_tour
+
+
+#' @importFrom tourr planned_tour
+#' @export
+tourr::planned_tour
