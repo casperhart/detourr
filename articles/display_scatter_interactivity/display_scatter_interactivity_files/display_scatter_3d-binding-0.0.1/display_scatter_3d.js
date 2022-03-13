71c116a6cd31e8f2368989f2a06c3ecaf37730f4
casperhart <casperhart@users.noreply.github.com>
2022-03-13 07:08:41 +0000

    Built site for detourr: 0.0.1@11c72cd

--- articles/display_scatter_interactivity/display_scatter_interactivity_files/display_scatter_3d-binding-0.0.1/display_scatter_3d.js
+++ articles/display_scatter_interactivity/display_scatter_interactivity_files/display_scatter_3d-binding-0.0.1/display_scatter_3d.js
@@ -1,18 +1,17 @@
 HTMLWidgets.widget({
+  name: "display_scatter_3d",
+  type: "output",
 
-    name: "display_scatter_3d",
-    type: "output",
-
-    factory: function (el, width, height) {
-        let scatter = new display_scatter_3d.DisplayScatter3d(el, width, height);
-        return {
-            renderValue: function (x) {
-                scatter.renderValue(x);
-            },
-            resize: function (width, height) {
-                scatter.resize(width, height)
-            },
-            s: scatter
-        }
-    }
-})
+  factory: function (el, width, height) {
+    let scatter = new display_scatter_3d.DisplayScatter3d(el, width, height);
+    return {
+      renderValue: function (x) {
+        scatter.renderValue(x);
+      },
+      resize: function (width, height) {
+        scatter.resize(width, height);
+      },
+      s: scatter,
+    };
+  },
+});
