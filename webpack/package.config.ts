4193ea6f849a2fc1a259a48c0e4c72c94943dcd1
casperhart <casperhart93@gmail.com>
2022-04-15 17:58:37 +1200

    ⚡ change tfjs backend to wasm

--- webpack/package.config.ts
+++ webpack/package.config.ts
@@ -22,6 +22,15 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.wasm$/i,
+        type: "javascript/auto",
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
   resolve: {
