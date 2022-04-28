c37e0553b2ec4e34d9b08353ef31cab6c6fc1923
casperhart <casperhart93@gmail.com>
2022-04-28 19:36:19 +1200

    ✨ add logic for offset slice in `show_slice()`

--- srcts/show_slice_2d/index.ts
+++ srcts/show_slice_2d/index.ts
@@ -4,10 +4,12 @@ import * as tf from "@tensorflow/tfjs-core";
 
 interface DisplaySlice2dConfig extends DisplayScatterConfig {
   epsilon: number;
+  anchor: number[];
 }
 
 export class DisplaySlice2d extends DisplayScatter2d {
   protected config: DisplaySlice2dConfig;
+  private anchor: tf.Tensor2D;
 
   constructor(containerElement: HTMLDivElement, width: number, height: number) {
     super(containerElement, width, height);
@@ -17,13 +19,26 @@ export class DisplaySlice2d extends DisplayScatter2d {
     this.brushButtonAction = null;
   }
 
+  protected preConstructPlotCallback(): void {
+    this.anchor = tf.expandDims(tf.tensor(this.config.anchor), 0);
+  }
+
   protected project(X: tf.Tensor2D, A: tf.Tensor2D): tf.Tensor2D {
     const projected = tf.matMul(X, A);
 
     // update point alphas for slice tour
+    // subtracting the anchor vector from each point vector is the equivalent of
+    // offsetting the projection plane so it passes through the anchor
+    // I.e. we offset the points by -anchor instead of the projection plane by + anchor
+    const X_prime = tf.sub(X, this.anchor);
     const dists = tf
       .sqrt(
-        tf.sum(tf.square(tf.sub(X, tf.matMul(projected, tf.transpose(A)))), 1)
+        tf.sum(
+          tf.square(
+            tf.sub(X_prime, tf.matMul(X_prime, tf.matMul(A, tf.transpose(A))))
+          ),
+          1
+        )
       )
       .dataSync();
 
