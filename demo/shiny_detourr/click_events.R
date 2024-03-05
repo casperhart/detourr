4a2005cfb347f760536c4b92a735071f13452194
Janith Wanniarachchi <janithcwanni@gmail.com>
2024-03-05 13:14:16 +1100

    :bug: fix demo for namespaced ids

---
+++ demo/shiny_detourr/click_events.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(detourr)
+
+ui <- function() {
+  fluidPage(
+    displayScatter3dOutput("detourr_out", width = "100%", height = "400px")
+  )
+}
+
+server <- function(input, output, session) {
+  output$detourr_out <- shinyRenderDisplayScatter2d({
+    detour(
+      tourr::flea |> 
+        dplyr::mutate(id = dplyr::row_number()), 
+      tour_aes(projection = -species, colour = species, label = id)
+    ) |>
+      tour_path(grand_tour(3), fps = 60) |>
+      show_scatter(alpha = 0.7, axes = TRUE)
+  })
+
+  observeEvent(input$detourr_out_detour_click, {
+    print(input$detourr_out_detour_click)
+  })
+}
+
+shinyApp(ui, server, options = list(port = 5534))
