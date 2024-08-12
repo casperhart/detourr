682699c0082d56c81fa50e40c33fb9f37fb9a3b7
janithwanni <janithcwanni@gmail.com>
2024-08-13 01:03:35 +0530

    :memo: update demo to show id on app

--- demo/shiny_detourr/click_events.R
+++ demo/shiny_detourr/click_events.R
@@ -3,7 +3,10 @@ library(detourr)
 
 ui <- function() {
   fluidPage(
-    displayScatter3dOutput("detourr_out", width = "100%", height = "400px")
+    fluidRow(
+      column(6,displayScatter3dOutput("detourr_out", width = "100%", height = "400px")),
+      column(6,textOutput("detourr_id"))
+    )
   )
 }
 
@@ -16,6 +19,11 @@ server <- function(input, output, session) {
     ) |>
       tour_path(grand_tour(3), fps = 60) |>
       show_scatter(alpha = 0.7, axes = TRUE)
+  })
+
+  output$detourr_id <- renderText({
+    req(!is.null(input$detourr_out_detour_click))
+    paste0("Clicked on id: ", input$detourr_out_detour_click)
   })
 
   observeEvent(input$detourr_out_detour_click, {
